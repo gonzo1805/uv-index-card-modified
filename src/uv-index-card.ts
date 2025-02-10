870ba012df653f5a3b679bf8aa39bf4585f4b1fd
gonzo1805 <gonzo1805@gmail.com>
2025-02-09 23:18:21 -0600

    Adding new image instead of the piramid

--- src/uv-index-card.ts
+++ src/uv-index-card.ts
@@ -131,19 +131,24 @@ export class UVIndexCard extends LitElement {
             <svg width="80%" viewBox="0 0 162 136" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
               <title>UV</title>
               <g id="UV-Index-Triangle" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                <polygon points="81.9537723 2.99975159 77.2979826 10.4602611 86.4956236 10.4362484" fill="${stateValue >= 12 ? colours.extreme : colours.idle}"></polygon>
-                <polygon points="92.8108692 20.7694268 70.8323051 20.8356688 76.2650231 12.1248408 87.5102538 12.0925478" fill="${stateValue >= 11 ? colours.extreme : colours.idle}"></polygon>
-                <polygon points="99.1192621 31.0946561 64.3589492 31.2022994 69.7916672 22.4914713 93.8186467 22.4177771" fill="${stateValue >= 10 ? colours.veryHigh : colours.idle}"></polygon>
-                <polygon points="105.434921 41.428828 57.8945103 41.5778726 63.3272282 32.8670446 100.134305 32.751121" fill="${stateValue >= 9 ? colours.veryHigh : colours.idle}"></polygon>
-                <polygon points="111.751405 51.7620892 51.4218149 51.9450828 56.8627892 43.2334268 106.442533 43.0769299" fill="${stateValue >= 8 ? colours.veryHigh : colours.idle}"></polygon>
-                <polygon points="118.058972 62.0882293 44.9567154 62.3192484 50.3894333 53.6092484 112.758356 53.4105223" fill="${stateValue >= 7 ? colours.high : colours.idle}"></polygon>
-                <polygon points="124.367447 72.4134586 38.4834421 72.686707 43.9244164 63.975879 119.066832 63.7440318" fill="${stateValue >= 6 ? colours.high : colours.idle}"></polygon>
-                <polygon points="130.17996 81.9276369 32.5388267 82.2331783 37.4513908 74.3512038 125.38216 74.0696752" fill="${stateValue >= 5 ? colours.moderate : colours.idle}"></polygon>
-                <polygon points="136.495618 92.2528662 26.0661313 92.6006369 31.4988492 83.8889809 131.195003 83.5759873" fill="${stateValue >= 4 ? colours.moderate : colours.idle}"></polygon>
-                <polygon points="142.804011 102.58621 19.6010318 102.96793 25.0337497 94.2562739 137.503396 93.9093312" fill="${stateValue >= 3 ? colours.moderate : colours.idle}"></polygon>
-                <polygon points="149.111661 112.912268 13.1285841 113.342841 18.5613021 104.632013 143.819302 104.242013" fill="${stateValue >= 2 ? colours.low : colours.idle}"></polygon>
-                <polygon points="155.427732 123.23758 6.66373231 123.717834 12.0964503 115.007006 150.127117 114.560701" fill="${stateValue >= 1 ? colours.low : colours.idle}"></polygon>
-                <polygon points="5.62342462 125.373554 0.999834872 132.792662 161.264189 132.792662 156.435014 124.893299" fill="${colours.low}"></polygon>
+                <!-- First Segment (Fully Rounded Left Corners) -->
+                <path d="M 10,10 H 45 V 40 H 10 Q 0,40 0,30 V 20 Q 0,10 10,10 Z" fill="${stateValue>=12? colours.extreme : colours.idle}"></path>
+
+                <!-- Middle Segments -->
+                <polygon points="50,10 95,10 95,40 50,40" fill="${stateValue>=11? colours.extreme : colours.idle}"></polygon>
+                <polygon points="100,10 145,10 145,40 100,40" fill="${stateValue>=10? colours.veryHigh : colours.idle}"></polygon>
+                <polygon points="150,10 195,10 195,40 150,40" fill="${stateValue>=9? colours.veryHigh : colours.idle}"></polygon>
+                <polygon points="200,10 245,10 245,40 200,40" fill="${stateValue>=8? colours.veryHigh : colours.idle}"></polygon>
+                <polygon points="250,10 295,10 295,40 250,40" fill="${stateValue>=7? colours.high : colours.idle}"></polygon>
+                <polygon points="300,10 345,10 345,40 300,40" fill="${stateValue>=6? colours.high : colours.idle}"></polygon>
+                <polygon points="350,10 395,10 395,40 350,40" fill="${stateValue>=5? colours.moderate : colours.idle}"></polygon>
+                <polygon points="400,10 445,10 445,40 400,40" fill="${stateValue>=4? colours.moderate : colours.idle}"></polygon>
+                <polygon points="450,10 495,10 495,40 450,40" fill="${stateValue>=3? colours.moderate : colours.idle}"></polygon>
+                <polygon points="500,10 545,10 545,40 500,40" fill="${stateValue>=2? colours.low : colours.idle}"></polygon>
+                <polygon points="550,10 595,10 595,40 550,40" fill="${stateValue>=1? colours.low : colours.idle}"></polygon>
+
+                <!-- Last Segment (Fully Rounded Right Corners) -->
+                <path d="M 600,10 H 640 Q 650,10 650,20 V 30 Q 650,40 640,40 H 600 V 10 Z" fill="${colours.low}"></path>
               </g>
             </svg>
           </div>
